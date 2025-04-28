d48d59518de5db4c95e91536f4528c2146c6e19f
SyedAhmedCU <syedtasnim.iutoic@gmail.com>
2025-04-28 07:46:56 -0600

    fix: update docker commands

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,6 @@ app.get('/fetch-news', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,  `0.0.0.0`, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
